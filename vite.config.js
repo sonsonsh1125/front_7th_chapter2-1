599e5edb2a91ae9e7833858a87d1c521282fb8e3
sonsonsh1125 <tmdgus8911@gmail.com>
2025-11-10 23:08:28 +0900

    fix: 배포오류 수정

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      },
+    },
+  },
+});
